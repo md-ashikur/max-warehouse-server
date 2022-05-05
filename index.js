54ad4ee0e44579a8111677dd34c0b6a0a3124217
MD Ashikur Rahman <71284265+WGDAshik@users.noreply.github.com>
2022-05-05 13:08:56 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+const app = express();
+// middleware
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.peb5q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('maxWareHouse').collection('product');
+
+        app.get('/product'), async(req, res) =>{
+            const query = {};
+            const cursor = productCollection.find(query);
+            const product = await cursor.toArray();
+            res.send(product);
+        }
+    }
+    finally { }
+}
+run.catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('sfsdsddsffffffff')
-  })
-  
-  app.listen(port, () => {
+    res.send('conncet')
+})
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
